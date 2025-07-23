b0465ca493efbfb9404c2fb15dfba8a604cea62a
muralikrishna0433-cyber <muralikrishna0433@email.com>
2025-07-23 13:43:13 +0530

    Initial setup

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
-  features = {}
-  subscription_id = var.subscription_id
+    subscription_id = var.subscription_id
+
+  features = {} #
 }
 
 resource "azurerm_resource_group" "rg" {
